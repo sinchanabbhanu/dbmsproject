e6096ad239ff80a0cef03b7927a6986161819a2f
Sinchana B <sinchanabbhanu4065@gmail.com>
2021-12-18 12:45:36 +0530

    some changes

---
+++ Site1/login.php
@@ -0,0 +1,36 @@
+<?php
+
+ session_start();
+ 
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $con = mysqli_connect("localhost", "root", "", "dbms");
+
+    if (!$con) {
+       die( " Connection failed. Please try again: Error: " . mysqli_connect_error() );
+    }
+
+    //to check if the username exists in the database
+
+    $username_existance_query = "SELECT upassword from login where usn = '".$username."';";
+    $query = mysqli_query($con, $username_existance_query);
+
+$query_result = mysqli_fetch_array($query);
+
+    // echo "$query_result[upassword] ";
+
+    if($query_result['upassword']  == $password)
+    {
+        header('Location: Page-3.html');  
+        mysqli_close($con);
+    }
+
+    else {
+        echo "Login failed. Please try again: Error: ";
+    }
+
+    mysqli_close($con);
+
+
+?>
