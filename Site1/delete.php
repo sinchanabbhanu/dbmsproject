0a154ad82c21f5e9f37386f8fde757c497e0f7ec
Sinchana B <sinchanabbhanu4065@gmail.com>
2021-12-25 19:08:06 +0530

    some changes

---
+++ Site1/delete.php
@@ -0,0 +1,27 @@
+<?php
+   
+   $con = mysqli_connect("localhost", "root", "", "dbms");
+
+   if (!$con) {
+     die(" Connection failed. Please try again: Error: " . mysqli_connect_error());
+   }
+
+   $usn =  $_REQUEST['usn'];
+
+  
+
+           $sql = array("login","status","user_details");
+           foreach($sql as $table) {
+           $query = "DELETE FROM $table WHERE usn = '$usn'";
+           mysqli_query($con,$query);
+          }
+
+   if (mysqli_query($con, $query)) {
+    echo "Record deleted successfully";
+  }
+   else {
+    echo "Error: " . $query . "<br>" . mysqli_error($con);
+  }
+  
+  mysqli_close($con);
+  ?>
